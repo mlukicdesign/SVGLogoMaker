d3dbedfa84d5c480c9579dab0735d7686eac1e94
mlukicdesign <mlukicdesign@gmail.com>
2023-05-26 23:28:53 +0800

    refractored code and added annotations. To Do: Apply test using Jest

--- index.js
+++ index.js
@@ -1,115 +1,106 @@
 // initialize dependencies
 const fs = require('fs');
 const inquirer = require('inquirer');
-
-// module import
 const { Shape, Circle, Square, Triangle } = require('./lib/shape.js');
 
-// SVG Class with constructor function to render output
 class SVGLogo {
-    constructor() {
-        this.textElement = '';
-        this.shapeElement = '';
-    }
+  constructor() {
+    this.textElement = ''; // Initialize the text element
+    this.shapeElement = ''; // Initialize the shape element
+  }
 
-    render() {
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
-    }
+  render() {
+    // Render the SVG markup by combining the shape element and text element
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+  }
 
-    setTextElement(color, text) {
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
-    }
+  setTextElement(color, text) {
+    // Set the text element with the provided color and text
+    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
 
-    setShapeElement(shape) {
-        this.shapeElement = shape.render();
-    }
+  setShapeElement(shape) {
+    // Set the shape element by rendering the provided shape
+    this.shapeElement = shape.render();
+  }
 }
 
-// Defines questions array for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter text (Up to 3 characters)',
-    },
-    {
-        type: 'input',
-        name: 'text-color',
-        message: 'What colour would you like your text to be? Enter a colour keyword or hexadecimal number',
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape to define your logo',
-        choices: ['Circle', 'Square', 'Triangle'],
-    },
-    {
-        type: 'input',
-        name: 'shape-color',
-        message: 'What colour would you like your shape to be? Enter a colour keyword or a hexadecimal number',
-    },
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter text (Up to 3 characters)',
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'What color would you like your text to be? Enter a color keyword or hexadecimal number',
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Choose a shape to define your logo',
+    choices: ['Circle', 'Square', 'Triangle'],
+  },
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'What color would you like your shape to be? Enter a color keyword or a hexadecimal number',
+  },
 ];
 
 function writeToFile(filename, content) {
-    fs.writeFile(filename, content, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(`Content written to ${filename} successfully.`);
-    });
-}
-
-// Async Function
-async function generateLogo() {
-    console.log('Generating Logo');
-
-    let userText = '';
-    let userTextColor = '';
-    let selectedShapeColor = '';
-    let selectedShape = '';
-
-    // Handle user input
-    const answers = await inquirer.prompt(questions);
-
-    userText = answers.text;
-    userTextColor = answers['text-color'];
-    selectedShapeColor = answers['shape-color'];
-    selectedShape = answers.shape.toLowerCase();
-
-    // Handle shape selection
-    let shape;
-
-    switch (selectedShape) {
-        case 'square':
-            shape = new Square();
-            console.log('User selected a square');
-            break;
-        case 'circle':
-            shape = new Circle();
-            console.log('User selected a circle');
-            break;
-        case 'triangle':
-            shape = new Triangle();
-            console.log('User selected a triangle');
-            break;
-        default:
-            console.log('An error has occurred');
-            return;
+  // Write the content to a file with the provided filename
+  fs.writeFile(filename, content, (err) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-
-
-    shape.setColor(selectedShapeColor);
-
-    const svg = new SVGLogo();
-    svg.setTextElement(userTextColor, userText);
-    svg.setShapeElement(shape);
-    const logoContent = svg.render();
-
-    writeToFile('logo.svg', logoContent);
-
-    console.log(userText)
-    console.log(userTextColor)
+    console.log(`Content written to ${filename} successfully.`);
+  });
 }
 
+async function generateLogo() {
+  console.log('Generating Logo');
+
+  // Prompt the user with questions and get the answers
+  const { text, textColor, shape, shapeColor } = await inquirer.prompt(questions);
+
+  let selectedShape;
+
+  // Create the selected shape based on user input
+  switch (shape.toLowerCase()) {
+    case 'square':
+      selectedShape = new Square();
+      console.log('User selected a square');
+      break;
+    case 'circle':
+      selectedShape = new Circle();
+      console.log('User selected a circle');
+      break;
+    case 'triangle':
+      selectedShape = new Triangle();
+      console.log('User selected a triangle');
+      break;
+    default:
+      console.log('An error has occurred');
+      return;
+  }
+
+  // Set the color of the selected shape
+  selectedShape.setColor(shapeColor);
+
+  const svg = new SVGLogo();
+  // Set the text and text color in the SVGLogo instance
+  svg.setTextElement(textColor, text);
+  // Set the shape in the SVGLogo instance
+  svg.setShapeElement(selectedShape);
+  // Render the final logo content
+  const logoContent = svg.render();
+
+  // Write the logo content to a file named 'logo.svg'
+  writeToFile('logo.svg', logoContent);
+}
+
+// Call the generateLogo function to start generating the logo
 generateLogo();
