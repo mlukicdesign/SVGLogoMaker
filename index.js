43e1d13cbb03481605a05077ca2cba2fc0af4584
mlukicdesign <mlukicdesign@gmail.com>
2023-05-26 23:12:40 +0800

    logo maker functionality now working, to-do: add testing using Jest, refractor code, annotate functionality

--- index.js
+++ index.js
@@ -3,130 +3,113 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // module import
-const {Circle, Square, Triangle} = require('./lib/shape.js');
+const { Shape, Circle, Square, Triangle } = require('./lib/shape.js');
 
-
-
-// SVG Class with constructor fucntion to render output
-
+// SVG Class with constructor function to render output
 class SVGLogo {
     constructor() {
-        this.textElement = ''
-        this.shapeElement = ''
+        this.textElement = '';
+        this.shapeElement = '';
     }
-    
+
     render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+    }
 
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    setTextElement(color, text) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
     }
-    setTextElement(text, color) {
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
+
     setShapeElement(shape) {
-        this.shapeElement = shape.render()
-
+        this.shapeElement = shape.render();
     }
-
 }
 
-
-
-
-
-
-
-
-// Defines questions arrays for user input
-
+// Defines questions array for user input
 const questions = [
     {
-        type: "input",
-        name: "text",
-        message: "Enter text (Up to 3 characters)",
+        type: 'input',
+        name: 'text',
+        message: 'Enter text (Up to 3 characters)',
     },
     {
-        type: "input",
-        name: "text-colour",
-        message: "What colour would you like your text to be? Enter a colour keyword or hexidecimal number",
+        type: 'input',
+        name: 'text-color',
+        message: 'What colour would you like your text to be? Enter a colour keyword or hexadecimal number',
     },
     {
-        type: "list",
-        name: "shape",
-        message: "Choose a shape to define your logo",
-        choices: ["Cricle", "Square", "Triangle"],
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape to define your logo',
+        choices: ['Circle', 'Square', 'Triangle'],
     },
     {
-        type: "input",
-        name: "shape-colour",
-        message: "Enter a colour keyword or a hexidecimal number",
+        type: 'input',
+        name: 'shape-color',
+        message: 'What colour would you like your shape to be? Enter a colour keyword or a hexadecimal number',
     },
-]
+];
 
 function writeToFile(filename, content) {
     fs.writeFile(filename, content, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`Content written to ${filename} successfully.`);
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`Content written to ${filename} successfully.`);
     });
-  }
-
-
+}
 
 // Async Function
-
 async function generateLogo() {
-    console.log('Generating Logo')
+    console.log('Generating Logo');
 
-    let userString = '';
-    let selectedTextColor = '';
+    let userText = '';
+    let userTextColor = '';
     let selectedShapeColor = '';
     let selectedShape = '';
 
-
-    // Handle user text input
-
+    // Handle user input
     const answers = await inquirer.prompt(questions);
 
-    if (answers.text.length > 0 && answers.text.length < 4) {
-        userString = answers.text;
-    } else {
-        console.log("Error! Please enter no more then 1 - 3 characters");
-        return;
-    }
-
-    // Apply users selected code
-
+    userText = answers.text;
+    userTextColor = answers['text-color'];
+    selectedShapeColor = answers['shape-color'];
+    selectedShape = answers.shape.toLowerCase();
 
     // Handle shape selection
+    let shape;
 
-    switch (selectedShape.toLowerCase()) {
-        case "square":
-            selectedShape = new Square();
+    switch (selectedShape) {
+        case 'square':
+            shape = new Square();
             console.log('User selected a square');
             break;
         case 'circle':
-            selectedShape = new Circle();
+            shape = new Circle();
             console.log('User selected a circle');
             break;
         case 'triangle':
-            selectedShape = new Triangle();
-            console.log('User selected Triangle');
+            shape = new Triangle();
+            console.log('User selected a triangle');
             break;
         default:
-            console.log('An error has occured')
+            console.log('An error has occurred');
+            return;
     }
 
-    // selectedShapeColor.setColor(selectedShapeColor)
+
+    shape.setColor(selectedShapeColor);
 
     const svg = new SVGLogo();
-    svg.setTextElement(userString, selectedTextColor);
-    svg.setShapeElement(selectedShape);
-    userString = svg.render();
+    svg.setTextElement(userTextColor, userText);
+    svg.setShapeElement(shape);
+    const logoContent = svg.render();
 
-    writeToFile();
-   
+    writeToFile('logo.svg', logoContent);
+
+    console.log(userText)
+    console.log(userTextColor)
 }
 
 generateLogo();
